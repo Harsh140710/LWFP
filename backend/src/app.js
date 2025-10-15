e0bfaeef2e8d68b0028d046eccdc24fd392c1dd9
Harsh140710 <sutharharsh108@gmail.com>
2025-10-15 12:49:12 +0530

    fix: add render frontend to CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,18 +10,27 @@ const app = express();
 import cors from "cors";
 
 const allowedOrigins = [
+  "http://localhost:5173",
   "https://timeless-elegancee-frontend.onrender.com",
-  "http://localhost:5173"
 ];
 
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 
 
