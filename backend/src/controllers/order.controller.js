556031161f8ed9abdf9e0450f8d938c342b9a350
Harsh140710 <sutharharsh108@gmail.com>
2026-04-10 11:08:11 +0530

    fix Product import in craete order controller

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,6 +1,7 @@
 // controllers/order.controller.js
 import { Order } from "../models/order.models.js";
-import { User } from "../models/user.models.js"; // Required import
+import { User } from "../models/user.models.js";
+import { Product } from "../models/product.models.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
